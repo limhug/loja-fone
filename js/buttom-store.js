0ccb029592ed459c9a288b654c1f9abe10d43359
Hugo Lima <hugo.hnl@gmail.com>
2019-08-30 01:03:18 -0300

    [013] - Desafio Básico

---
+++ js/buttom-store.js
@@ -0,0 +1,1 @@
+const $buttonAdicionar = document.querySelector(".-second");
---
+++ js/description-product.js
@@ -0,0 +1,7 @@
+const $heart = window.document.querySelector(".heart");
+
+$heart.addEventListener('click', handelClick)
+
+function handelClick(){
+  console.log('clicou!!!')
+}
